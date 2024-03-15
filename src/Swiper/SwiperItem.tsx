f3326602cbb73a4777855ffe8cee0e1a81f3d6f4
Cody Christ <cchrist1210@gmail.com>
2024-03-15 13:46:33 +0100

    fix: added classname prop to swiperitem

--- src/Swiper/SwiperItem.tsx
+++ src/Swiper/SwiperItem.tsx
@@ -1,16 +1,19 @@
 import React, { JSXElementConstructor, ReactElement } from 'react';
 
 import styles from './SwiperItem.module.css';
+import clsx from 'clsx';
 
 export type SwiperItemType = {
   children: ReactElement<any, string | JSXElementConstructor<any>>;
+  className?: string;
   numberOfItemsPerSwipe?: number;
   onClick?: React.MouseEventHandler<HTMLLIElement>;
 }
 
-function SwiperItem({ children, numberOfItemsPerSwipe = 1, onClick }: SwiperItemType) {
+function SwiperItem({ children, className, numberOfItemsPerSwipe = 1, onClick }: SwiperItemType) {
+  const listClasses = clsx(styles["swiper-item"], className);
   return (
-    <li className={styles["swiper-item"]} style={{width: `${100 / numberOfItemsPerSwipe }%`}} onClick={onClick}>
+    <li className={listClasses} style={{width: `${100 / numberOfItemsPerSwipe }%`}} onClick={onClick}>
       <div className={styles["swiper-item-inner-container"]} draggable={false}>
         {children}
       </div>
